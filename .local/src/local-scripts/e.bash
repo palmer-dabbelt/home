09cc14a08c91223567cba0311ff64fce175abaeb
Palmer Dabbelt <palmer@dabbelt.com>
2015-11-21 00:07:05 -0800

    Background urxvt on e
    This matches the old e behavior, which is probably in /usr/bin/e on my
    machines.

--- .local/src/local-scripts/e.bash
+++ .local/src/local-scripts/e.bash
@@ -2,7 +2,7 @@
 
 if [[ "$DISPLAY" != "" ]]
 then
-	urxvt -e vim "$(readlink -f $1)"
+	urxvt -e vim "$(readlink -f $1)" &
 elif [[ "$TMUX" != "" ]]
 then
 	tmux split-window -h "exec vim $(readlink -f $1)"
