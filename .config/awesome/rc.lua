835047f9516c610d8a7ed6ff8dd6d8c165750931
Palmer Dabbelt <palmer@dabbelt.com>
2016-02-13 23:03:48 -0800

    Add the Win+R box back to awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -162,7 +162,7 @@ for s = 1, screen.count() do
     local left_layout = wibox.layout.fixed.horizontal()
     --left_layout:add(mylauncher)
     --left_layout:add(mytaglist[s])
-    --left_layout:add(mypromptbox[s])
+    left_layout:add(mypromptbox[s])
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
