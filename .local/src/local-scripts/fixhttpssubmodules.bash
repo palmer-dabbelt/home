1080e17500bf301fa8262a2b6eee17d01b527591
Palmer Dabbelt <palmer@dabbelt.com>
2016-06-16 17:16:56 -0700

    Also fix git:// submodules

--- .local/src/local-scripts/fixhttpssubmodules.bash
+++ .local/src/local-scripts/fixhttpssubmodules.bash
@@ -4,3 +4,4 @@ git_config="$(GIT_EDITOR=echo git config -e)"
 
 echo "Fixing https submodule pointers in $git_config"
 sed 's!https://github.com/!git@github.com:!g' -i $git_config
+sed 's!git://github.com/!git@github.com:!g' -i $git_config
