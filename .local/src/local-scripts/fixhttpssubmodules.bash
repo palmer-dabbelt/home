d6472e0ea2f9ecc99a26628ac727baddeedce77b
Palmer Dabbelt <palmer@dabbelt.com>
2017-03-07 17:08:50 -0800

    Un-break fixhttpssubmodules

--- .local/src/local-scripts/fixhttpssubmodules.bash
+++ .local/src/local-scripts/fixhttpssubmodules.bash
@@ -3,5 +3,5 @@
 git_config="$(GIT_EDITOR=echo git config -e)"
 
 echo "Fixing https submodule pointers in $git_config"
-sed 's!https://[a-z.]/!git@github.com:!g' -i $git_config
-sed 's!git://[a-z.]/!git@github.com:!g' -i $git_config
+sed 's!https://github.com/!git@github.com:!g' -i $git_config
+sed 's!git://github.com/!git@github.com:!g' -i $git_config
