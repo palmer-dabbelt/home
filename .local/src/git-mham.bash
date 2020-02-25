3339a0668b3186a2c47de77e3042c9d80229e097
Palmer Dabbelt <palmerdabbelt@google.com>
2020-02-25 15:56:04 -0800

    Auto review
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- .local/src/git-mham.bash
+++ .local/src/git-mham.bash
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+tags=""
+if [[ "$1" == "--reviewed-by" ]]
+then
+    tags="${tags}r"
+fi
+
 mhng-pipe-scan "$@" | sort -k 4 | cut -d' ' -f1 | while read seqnum
 do
-    mhng-pipe-show_stdout "$seqnum" | git am -3sS
+    mhng-pipe-show_stdout "$seqnum" | grep -ve "^Cc" | git am -3S
+
+    mhng-pipe-show_stdout "$seqnum" --thread | grep -e "^Reviewed-by: " | while read tag
+    do
+        echo "Adding $tag"
+        git add-tag "$tag"
+    done
+
+    tag "$tags" | while read tag
+    do
+        echo "Adding $tag"
+        git add-tag "$tag"
+    done
+
+    (
+        export GIT_EDITOR=true
+        git commit --amend -s --quiet
+    )
 done
